fdd39b642cace507bd20443d8c2ff16da783f7c4
deepak90221 <116907628+deepak90221@users.noreply.github.com>
2026-08-29 21:21:25 +0530

    added home controller

---
+++ src/main/java/com/JobPortal/SpringJobportal/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.JobPortal.SpringJobportal.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot Job Portal is working!";
+    }
+    
+}
+
+
